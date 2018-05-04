43a69c3be78af8d5bb4b1ab8b7c68eb541071cfc
Fadli Al Baihaqi <alibaihaqi1704@gmail.com>
2018-05-04 15:27:10 +0700

    update tugas lagi

--- exercise-8.js
+++ exercise-8.js
@@ -2,13 +2,9 @@ function pasanganTerbesar (num) {
   var strnum = String(num).split('');
   var max = Number(strnum.slice(0,2).join(''));
 
-  for (var i = 0; i <= num.length - 1; i++) {
-    var num2 = Number(strnum.slice(i+1,i+3).join(''));
-    
-    if (max > num2) {
-      max = max;
-    }
-    else {
+  for(var i = 0; i <= strnum.length - 2; i++) {
+    var num2 = Number(strnum.slice(i,i+2).join(''))
+    if (max < num2) {
       max = num2;
     }
   }
