35c382819608c4c9afb5f094864a5533f24a8202
Fadli Al Baihaqi <alibaihaqi1704@gmail.com>
2018-05-05 08:55:44 +0700

    update tugas 15-1

--- exercise-15.js
+++ exercise-15.js
@@ -2,27 +2,24 @@
 
 //   // you can only write your code here!
 // }
-var animals = ['cacing', 'ayam', 'kuda', 'anoa', 'kancil'];
-var animals1 = animals.sort();
-var grup = [];
-for (var i = 0; i <= animals1.length - 1; i++) {
-  var animalsSeabjad = [];
-  var hurufAwal = animals1[i][0];
-  var urutan = [animals1[i]];
-  for (var j = animals1.length - 1; j > i; j--) {
-    if (hurufAwal === animals1[j][0]) {
-      urutan.push(animals1[j]);
-      animals1.splice(j,1);
+function groupAnimals(animals) {
+  var animals1 = animals.sort();
+  var grup = [];
+  for (var i = 0; i <= animals1.length - 1; i++) {
+    var hurufAwal = animals1[i][0];
+    var urutan = [animals1[i]];
+    for (var j = animals1.length - 1; j > i; j--) {
+      if (hurufAwal === animals1[j][0]) {
+        urutan.push(animals1[j]);
+        animals1.splice(j,1);
+      }
     }
-
+  grup.push(urutan);
   }
-  animalsSeabjad.push(urutan);
-  grup.push(animalsSeabjad);
-
+  return grup;
 }
-console.log(grup)
 // TEST CASES
-// console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil']));
+console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil']));
 // [ ['ayam', 'anoa'], ['cacing'], ['kuda', 'kancil'] ]
-// console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil', 'unta', 'cicak' ]));
+console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil', 'unta', 'cicak' ]));
 // [ ['ayam', 'anoa'], ['cacing', 'cicak'], ['kuda'], ['unta'] ]
